64960259c3f2e6985524b97cd4b814ea15f329fe
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-09-21 17:52:21 +0300

    ДЗ 30.2-4 Настроена фильтрацию для эндпоинта вывода списка платежей.

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "phonenumber_field",
+    "django_filters",
     "users",
     "materials",
 ]
@@ -54,6 +55,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 DATABASES = {
     "default": {
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+from users.models import Payment
+from users.serializers import PaymentSerializer
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    """
+    Получение списка всех платежей.
+
+    Возвращает перечень платежей с базовой информацией.
+    Позволяет администраторам и пользователям просматривать историю оплат.
+    """
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["payment_method", "course_paid", "single_lesson_paid"]
+    ordering_fields = ["payment_date",]
