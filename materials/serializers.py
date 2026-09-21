0efef49870b18f6631715f710109d9b397ef1011
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-09-21 12:46:43 +0300

    ДЗ 30.2-1 Для модели курса добавлено в сериализатор поле вывода количества уроков.

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,18 +1,7 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    """
-    Сериализатор для модели курса.
-
-    Используется для полного преобразования данных курса (включая ID, название,
-    описание и превью) в формат JSON при чтении и записи.
-    """
-    class Meta:
-        model = Course
-        fields = "__all__"
 
 
 class LessonSerializer(ModelSerializer):
@@ -22,6 +11,27 @@ class LessonSerializer(ModelSerializer):
     Обеспечивает валидацию и преобразование данных урока, включая валидацию
     внешнего ключа связи с родительским курсом.
     """
+
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(ModelSerializer):
+    """
+    Сериализатор для модели курса.
+
+    Выводит базовую информацию о курсе и автоматически рассчитывает
+    общее количество связанных с ним уроков.
+    """
+    lessons_count = SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = ["name", "description", "lessons_count"]
+
+    def get_lessons_count(self, course):
+        """
+        Возвращает общее количество уроков, привязанных к данному курсу.
+        """
+        return course.lessons.count()
