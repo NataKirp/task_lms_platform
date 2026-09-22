1d78d8aba514de450e351ed7750a499337839945
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-09-22 22:22:07 +0300

    ДЗ 31.1 Реализован CRUD для пользователей, настроена JWT-авторизация.

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,46 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import Payment
+from users.models import Payment, User
+
+
+class UserRegisterSerializer(ModelSerializer):
+    """
+    Сериализатор для РЕГИСТРАЦИИ пользователя.
+    """
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "phone_number", "city"]
+        extra_kwargs = {"password": {"write_only": True}}  # Пароль будет приниматься, но не будет отдаваться в JSON
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")  # Извлекаем пароль из валидированных данных
+        user = User(**validated_data)  # Создаем пользователя со всеми оставшимися полями
+        user.set_password(password)  # Хэшируем и сохраняем пароль
+        user.save()
+        return user
+
+
+class UserProfileSerializer(ModelSerializer):
+    """
+    Сериализатор для СВОЕГО профиля (Доп. задание №2).
+    Включает полную информацию, фамилию и историю платежей.
+    """
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name", "phone_number", "city", "avatar"]
+
+
+class UserReadOnlySerializer(ModelSerializer):
+    """
+    Сериализатор для ЧУЖОГО профиля (Доп. задание №3).
+    Скрывает пароль, фамилию и историю платежей. Доступна только общая информация.
+    """
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "city"]
 
 
 class PaymentSerializer(ModelSerializer):
--- users/views.py
+++ users/views.py
@@ -1,9 +1,36 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-from users.models import Payment
-from users.serializers import PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserRegisterSerializer, UserProfileSerializer, UserReadOnlySerializer
+
+
+class UserViewSet(ModelViewSet):
+    """
+    ViewSet для управления пользователями (CRUD).
+
+    Обеспечивает регистрацию, просмотр, редактирование и удаление профилей.
+    """
+
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return UserRegisterSerializer
+        if self.action in ["retrieve", "update", "partial_update"]:
+            if self.get_object() == self.request.user:
+                return UserProfileSerializer
+        return UserReadOnlySerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            # super().get_permissions() ожидает, что элементы внутри этого кортежа являются объектами
+            # (экземплярами классов), а не самими классами. То есть нужно инициализировать класс скобками: AllowAny()
+            return [AllowAny()]
+        return super().get_permissions()
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -17,4 +44,4 @@ class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ["payment_method", "course_paid", "single_lesson_paid"]
-    ordering_fields = ["payment_date",]
+    ordering_fields = ["payment_date", ]
